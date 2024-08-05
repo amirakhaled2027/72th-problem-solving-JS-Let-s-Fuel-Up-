e5b687797f655b35f33f748dbf930b3d3c67f4f7
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-08-05 23:03:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+// 10 * distance
+// storedFuel => 100
+
+// if storedFuel => 100 ... "you're ready to set of"
+// if storedFuel => 100 ... distance 10 kilos
+
+// if storedFuel => 100 --- return distance 
+// else ----- return distance * 10
+
+
+//SIMPLY 
+//return 100 ---- clacFuel < 100
+
+
+
+function calcFuel(distance) {
+   if (calcFuel < 100) {
+    return distance;
+   } 
+   else {
+    return distance * 10;
+   }
+}
+console.log(calcFuel(15));
+console.log(calcFuel(23.5));
+console.log(calcFuel(3));
+
+
+
+
